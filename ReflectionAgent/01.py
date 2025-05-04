04c728a84c009ee760baf991b052221d23f5df7c
Alok-Kumar2005 <ay747283@gmail.com>
2025-05-04 18:40:23 +0530

    reflection comp

---
+++ ReflectionAgent/01.py
@@ -0,0 +1,21 @@
+#### some comments
+"""
+Reflection means how we are looking at our reflection in the mirror, reflection means looking at yourself or your actions
+for example
+- After giving a presentation, thinking about how it went
+- After writing an email, reading it again to check if it's clear
+- After making a decision, considiring if it was the right choise
+
+
+So a reflection agent pattern is an AI system pattern that can look at its own 
+outputs and think about them/make it better- just like how we look at
+ourselves in a mirror and self-reflect, make ourselves better
+
+
+Three types of reflection agents:
+- 1.Basic Reflection Agent
+- 2.Reflexion Agents
+- 3.Language Agent tree seach
+
+
+"""
